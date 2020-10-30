411809f97e89edbda75617128ed71847bf467eff
Cameron Bisca <cjbisca@gmail.com>
2020-10-30 22:54:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,14 +2,9 @@ require 'pry'
 
 
 def snake_it_up(string)
-  if (string[0] == "s")
-    count = 0
-    while count < 10
-      string = "s" + string
-      count += 1
-      string
-    end
-  else 
+  if string[0] == "s"
+    puts "ssssssssss#{string}"
+   else 
     string
   end
 end 
